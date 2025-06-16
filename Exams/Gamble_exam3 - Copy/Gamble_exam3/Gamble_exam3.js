35b31257e7ee09ba166db65553a6f4b07042a235
Carlton-Gamble <cg21697@georgiasouthern.edu>
2025-06-16 08:08:54 -0400

    Add files via upload

---
+++ Exams/Gamble_exam3 - Copy/Gamble_exam3/Gamble_exam3.js
@@ -0,0 +1,17 @@
+/*
+
+	IT 1430 - Web Page Development
+	Fall 2019
+	Exam 3 Part 2
+
+	Exam 3 JavaScript 
+	Author: Carlton Gamble
+	Date:    4/24/2024
+
+	Filename: lastName_exam3.js
+
+*/
+
+function mouse(inputParameter){
+	alert(inputParameter);
+}
